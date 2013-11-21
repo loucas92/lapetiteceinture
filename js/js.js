dead5dd2949f5dd1453e2ed777fcf3d122bc0b8b
Baltha <adrien.bellonserre@hotmail.fr>
2013-11-21 16:08:18 +0100

    vide

--- js/js.js
+++
@@ -1,7 +0,0 @@
-jQuery.fn.spectragram.accessData = {
-    accessToken: '[587779553.f0eb2ab.a48d82f195a440079e4022a8ef4d3b05]',
-    clientID: '[f0eb2ab0bedd403a999d0936834681ff]',
-};
-
-$('insta').spectragram('getPopular');
-});
