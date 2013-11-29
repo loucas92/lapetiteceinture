57ff0f54f7d6547efae7b8a3688fabcf5d5632ca
Baltha <adrien.bellonserre@hotmail.fr>
2013-11-29 13:52:56 +0100

    mvc js amélioration

--- Pc/js/scrolling.js
+++ Pc/js/scrolling.js
@@ -21,5 +21,32 @@ var scrolling={
 			      duration:1000,
 			      hash:true 
 			    });
-        }
+        },
+    mute : function(){
+    	if($("#mute").is(".mute"))
+		{
+			$(".mute").attr("class", "sound");
+			$("#audio")[0].play();
+		}
+		else
+		{
+			$(".sound").attr("class", "mute");
+			$("#audio")[0].pause();
+		}
+		
+    }
 }
+
+$(document).ready(function(){
+	
+	$(".scroll").mCustomScrollbar({
+                    verticalScroll:true, // barre verticale
+                    theme:"light",
+                    scrollInertia: 13,
+                    scrollButtons:{
+                            enable: false
+
+                    }
+                });
+	
+	});
